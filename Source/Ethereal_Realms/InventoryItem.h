095b9f7d1e01668da794470f9222be7c8a9fa8d1
Mohamed Nour <71439813+MohamedNourSadek@users.noreply.github.com>
2023-05-20 20:34:31 +0300

    Inventory Items

---
+++ Source/Ethereal_Realms/InventoryItem.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/Image.h"
+#include "InventoryItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ETHEREAL_REALMS_API UInventoryItem : public UImage
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere) float testValue = 0.0;
+};
